294e25a25d4e744ac2543cb8b7058d545c09ad82
Hector Salgado <53808824+hector323@users.noreply.github.com>
2020-11-29 15:17:55 -0800

    modified docs and build.py

--- build.py
+++ build.py
@@ -1,51 +1,76 @@
-# You should have a “pages” list in the following format:4
-
-# Your list must contain dictionaries. Each dictionary has information3 about a page on your site. It must also contain a file path that can be used to read the contents of the page, and a file path of where you will eventually output the combined version of this page.
+# list of all content files
 pages = [
   {
-  "filename": "content/about.html", "output": "docs/about.html", "title": "About Me"
+  "filename": "content/blog.html", "output": "/docs/index.html", "title": "My Personal Blogs"
   },
   {
-  "filename": "content/blog.html", "output": "docs/blog.html", "title": "My programming blog"
+  "filename": "content/projects.html", "output": "/docs/index.html", "title": "Projects"
   },
   {
-  "filename": "content/blog.html", "output": "docs/blog.html", "title": "My programming blog"
+  "filename": "content/contact.html", "output": "/docs/index.html", "title": "About Me"
   }
 ]
 
-for page in pages:
-  # print(page['filename'])
-  # To access data within a dictionary, consider the following code as a clue:
-  # Where "page" is a dictionary with a key "title"
-  print(page) # Replace this line eventually with other stuff...
 
-# Where "page" is a dictionary with a key "title"
-  page_title = page['filename']
-  print(page_title)
+def content():
+  for page in pages:
+      content = page['filename']
+      return content
+
+content()
+
+def apply_template(content):
+  for page in pages:
+      page_title = page['filename']
+      template = open("./templates/base.html").read()
+    # Read in the content of the index HTML page
+      index_content = open(page_title).read()
+      finished_index_page = template.replace("{{content}}", index_content)
+      results = open("./docs/index.html", "w+").write(finished_index_page)
+
+  return results
+
+apply_template(content)
+
+def main():
+  for page in pages:
+    page_title = page['filename']
+    # Read in the entire template
+    template = open("./templates/base.html").read()
+    # Read in the content of the index HTML page
+    index_content = open(page_title).read()
+    finished_index_page = template.replace("{{content}}", index_content)
+
+    return finished_index_page
+
+main()
+
+
+
+# def content():
+#   for page in pages:
+#     page_title = page['filename']
+#     template = open("./templates/base.html").read()
+#     # Read in the content of the index HTML page
+#     index_content = open(page_title).read()
+#     finished_index_page = template.replace("{{content}}", index_content)
+
+#     return finished_index_page
+
+# content()
 
 # def apply_template(content):
-#   # TODO: Read in template, do string replacing, and return result
+#   for page in pages:
+#     page['output'].replace(x, y)
+#   results = open("docs/index.html", "w+").write(content)
+#   # print(results)
 #   return results
 
+
 # def main():
-#   content = open(’docs/index.html’).read()
+#   content = open("docs/index.html").read()
 #   resulting_html_for_index = apply_template(content)
-
-# def main():
-#   base_html = open('./templates/base.html').read()
-#   middle_html = open('./content/blog.html').read()
-#   combined_html = base_html + middle_html
-#   open('blog.html', 'w+').write(combined_html)
-
-#   middle_html = open('./content/projects.html').read()
-#   combined_html = base_html + middle_html
-#   open('projects.html', 'w+').write(combined_html)
-
-#   middle_html = open('./content/contact.html').read()
-#   combined_html = base_html + middle_html
-#   open('contact.html', 'w+').write(combined_html)
+#   return resulting_html_for_index
 
 # main()
-
-
 
