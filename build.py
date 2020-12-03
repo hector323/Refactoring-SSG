fc5a1216b4e4ecfe6e5ed3f2e08ea73c10c28fa1
Hector Salgado <53808824+hector323@users.noreply.github.com>
2020-12-03 10:15:36 -0800

    updated python script

--- build.py
+++ build.py
@@ -1,112 +1,47 @@
 # list of all content files
 pages = [
   {
-  "filename": "content/blog.html", "output": "/docs/index.html", "title": "My Personal Blogs"
+  "filename": "content/blog.html", "output": "docs/index.html", "title": "My Personal Blogs"
   },
   {
-  "filename": "content/projects.html", "output": "/docs/index.html", "title": "Projects"
+  "filename": "content/projects.html", "output": "docs/index.html", "title": "Projects"
   },
   {
-  "filename": "content/contact.html", "output": "/docs/index.html", "title": "About Me"
+  "filename": "content/contact.html", "output": "docs/index.html", "title": "About Me"
   }
 ]
 
-
-def content():
-  for page in pages:
-      content = page['filename']
-      return content
-
-content()
-
-def apply_template(content):
-  for page in pages:
-      page_title = page['filename']
-      template = open("./templates/base.html").read()
-    # Read in the content of the index HTML page
-      index_content = open(page_title).read()
-      finished_index_page = template.replace("{{content}}", index_content)
-      results = open("./docs/index.html", "w+").write(finished_index_page)
-
-  return results
-
-apply_template(content)
-
-def main():
-  for page in pages:
-    page_title = page['filename']
-    # Read in the entire template
-    template = open("./templates/base.html").read()
-    # Read in the content of the index HTML page
-    index_content = open(page_title).read()
-    finished_index_page = template.replace("{{content}}", index_content)
-
-    return finished_index_page
-
-main()
-
-
-
-
+# Get the main base template
 # First, get the template files
-template = open('/templates/base.html').read()
+# template = open('./templates/base.html').read()
+def file_content(page_title):
+  base_template = open("./templates/base.html").read()
+  # replace title with page_title
+  template = base_template.replace("{{title}}", page_title)
+  return template
 
 # Read in index HTML code
-content = open('middle_index.html').read()
+# needs both template and content pages
+def apply_template(template, content):
+  # apply content page with template with using a placeholder {{content}} in the body
+  base_template = open("./templates/base.html").read()
+  completed_page = template.replace("{{content}}", content)
+  return completed_page
 
-# Combine template HTML code with content HTML code
-index_html = top_template + content
-open('index.html', 'w+').write(index_html)
+# loop through each page
+def main():
+  for page in pages:
+    page_content = page['filename']
+    page_output = page['output']
+    page_title = page['title']
 
-# Rinse and repeat, but with blog HTML code
-content = open('middle_blog.html').read()
-blog_html = top_template + content + bottom_template
-open('blog.html', 'w+').write(blog_html)
+    # Read in the entire template
+    template = file_content(page_title)
+    content = open(page_content).read()
+    # Read in the content of the index HTML page
+    completed_page = apply_template(template, content)
+    open(page_output, "w+").write(completed_page)
 
-# And again, this time with art HTML code
-content = open('middle_art.html').read()
-art_html = top_template + content + bottom_template
-open('art.html', 'w+').write(art_html)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def content():
-#   for page in pages:
-#     page_title = page['filename']
-#     template = open("./templates/base.html").read()
-#     # Read in the content of the index HTML page
-#     index_content = open(page_title).read()
-#     finished_index_page = template.replace("{{content}}", index_content)
-
-#     return finished_index_page
-
-# content()
-
-# def apply_template(content):
-#   for page in pages:
-#     page['output'].replace(x, y)
-#   results = open("docs/index.html", "w+").write(content)
-#   # print(results)
-#   return results
-
-
-# def main():
-#   content = open("docs/index.html").read()
-#   resulting_html_for_index = apply_template(content)
-#   return resulting_html_for_index
-
-# main()
-
+# invoke the main function
+if __name__ == "__main__":
+    	main()
